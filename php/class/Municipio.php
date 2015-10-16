2e6b36189aa0f21abf5da92e03f946739ff31b7a
WEBXICO-MX <weiss.uttab@gmail.com>
2015-10-16 09:43:55 -0500

    Atributo $cve_estado se cambio por Tipo Estado.

--- php/class/Municipio.php
+++ php/class/Municipio.php
@@ -8,7 +8,9 @@
 require_once 'UtilDB.php';
 
 class Municipio {
-
+    /**
+    * @var Estado $cve_estado Tipo Estado
+    */        
     private $cve_estado;
     private $cve_municipio;
     private $nombre;
@@ -30,14 +32,14 @@ class Municipio {
 
     function __construct1($cveEstado, $cveMunicipio) {
         $this->limpiar();
-        $this->cve_estado = $cveEstado;
+        $this->cve_estado = new Estado($cveEstado);
         $this->cve_municipio = $cveMunicipio;
         $this->cargar();
     }
 
     private function limpiar() {
 
-        $this->cve_estado = 0;
+        $this->cve_estado = NULL;
         $this->cve_municipio = 0;
         $this->nombre = "";
         $this->activo = false;
@@ -50,7 +52,7 @@ class Municipio {
 
         if (!$this->_existe) {
             $this->cve_municipio = UtilDB::getSiguienteNumero("municipios", "cve_municipioo");
-            $sql = "INSERT INTO municipios VALUES($this->cve_estado,$this->cve_municipio,'$this->nombre',$this->activo)";
+            $sql = "INSERT INTO municipios VALUES(".$this->cve_estado->getCve_estado().",$this->cve_municipio,'$this->nombre',$this->activo)";
             $count = UtilDB::ejecutaSQL($sql);
             if ($count > 0) {
                 $this->_existe = true;
@@ -58,20 +60,20 @@ class Municipio {
         } else {
             $sql = "UPDATE municipios SET ";
             $sql.= "nombre = '$this->nombre',";
-            $sql.= "cve_estado = '$this->cve_estado',";
+            $sql.= "cve_estado = ".$this->cve_estado->getCve_estado().",";
             $sql.= "activo=" . ($this->activo ? "1" : "0");
-            $sql.= " WHERE cve_estado = $this->cve_estado AND cve_municipio = $this->cve_municipio";
+            $sql.= " WHERE cve_estado = ".$this->cve_estado->getCve_estado()." AND cve_municipio = $this->cve_municipio";
             $count = UtilDB::ejecutaSQL($sql);
         }
         return $count;
     }
 
     function cargar() {
-        $sql = "SELECT * FROM municipios WHERE cve_estado = $this->cve_estado AND cve_municipio = $this->cve_municipio";
+        $sql = "SELECT * FROM municipios WHERE cve_estado = ".$this->cve_estado->getCve_estado()." AND cve_municipio = $this->cve_municipio";
         $rst = UtilDB::ejecutaConsulta($sql);
 
         foreach ($rst as $row) {
-            $this->cve_estado = $row['cve_estado'];
+            $this->cve_estado = new Estado($row['cve_estado']);
             $this->cve_estado = $row['cve_municipio'];
             $this->nombre = $row['nombre'];
             $this->activo = $row['activo'];
@@ -81,11 +83,14 @@ class Municipio {
     }
 
     function borrar() {
-        $sql = "DELETE FROM municipios WHERE cve_estado = $this->cve_estado AND cve_municipio = $this->cve_municipio";
+        $sql = "DELETE FROM municipios WHERE cve_estado = ".$this->cve_estado->getCve_estado()." AND cve_municipio = $this->cve_municipio";
         $count = UtilDB::ejecutaSQL($sql);
         return $count;
     }
 
+    /**
+    * @return Estado Devuelve tipo Estado
+    */
     function getCve_estado() {
         return $this->cve_estado;
     }
@@ -105,7 +110,12 @@ class Municipio {
     function get_existe() {
         return $this->_existe;
     }
-
+     /**
+     * Establece el estado del municipio.
+     *
+     * @param Estado $cve_estado Objeto de la clase Estado
+     *
+     */
     function setCve_estado($cve_estado) {
         $this->cve_estado = $cve_estado;
     }
