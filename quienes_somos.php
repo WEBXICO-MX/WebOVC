ba0dbd07ea9b26f5a3c1c05251eebbf76acaf437
WEBXICO-MX <weiss.uttab@gmail.com>
2015-10-21 16:01:56 -0500

    Versión inicial.

---
+++ quienes_somos.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Grupo HISA | Home</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Roberto Eder Weiss Juárez">
+        <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+        <link href="twbs/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/heroic-features.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- Navigation -->
+        <?php require_once 'php/include_header.php'; ?>
+
+        <!-- Page Content -->
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>¿Quienes somos?</h1>
+                    <p>Somos una empresa tabasqueña, pero con una óptica mundial, que inicia en el año 1996, en materia de capacitación en general, tomando en cuenta el aspecto conductual, refiriéndonos a la actitud y a la aptitud, la motivación y la autoestima; buscando desarrollar y potenciar  al recurso humano como fuerza trabajadora, para que en consecuencia, eleve su rendimiento laboral, y propicie resultados óptimos y satisfactorios a la empresa.</p>
+                    
+                    <h2>Política de calidad</h2>
+                    <p>Grupo HISA se centra en un mejoramiento continuo de la calidad, entendido como la implementación de acciones sistemáticas y continuas de calidad, con enfoque en procesos y en el  cliente/usuario, manifestando este, en brindar al usuario  calidez, una atención oportuna y enfocada en el aseguramiento de la eficiencia de los eventos de consultoría, asistencia técnica y capacitación.</p>
+                    
+                    <h2>Valores</h2>
+                    <ul>
+                        <li>La Responsabilidad</li>
+                    <li>La Atención</li>
+                    <li>La Calidad </li>
+                    <li>La Dedicación</li>
+                    <li>La Educación</li>
+                    <li>El Desarrollo Personal</li>
+                    <li>El Desarrollo Profesional</li>
+                    <li>La Efectividad</li>
+                    <li>El Respeto</li>
+                    <li>La Seriedad</li>
+                    <li>La Formalidad</li>
+                    <li>La Integridad</li>
+                      </ul>                  
+                    
+                </div>
+            </div>
+            <hr>
+            <?php require_once 'php/include_footer.php'; ?>
+        </div>
+        <!-- /.container -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpdw9gyXrQvIvyLrVi9FneyumQOE8_9CA&sensor=true"></script>
+        <script src="js/maps.js"></script>
+        <script src="js/jQuery/jquery-1.11.3.min.js"></script>
+        <script src="twbs/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+        <script src="js/ovc.js"></script>
+    </body>
+</html>
