d58dfde1638b183c183695fb4503988b11489352
WEBXICO-MX <weiss.uttab@gmail.com>
2015-10-21 16:02:00 -0500

    Versión inicial.

---
+++ organigrama.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Grupo HISA | Organigrama</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Roberto Eder Weiss Juárez">
+        <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+        <link href="twbs/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/heroic-features.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- Navigation -->
+        <?php require_once 'php/include_header.php'; ?>
+
+        <!-- Page Content -->
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Organigrama</h1>
+                    <img src="img/organigrama.png" alt="Organigrama" class="img-responsive"/>
+                </div>
+            </div>
+
+            <hr>
+            <?php require_once 'php/include_footer.php'; ?>
+        </div>
+        <!-- /.container -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpdw9gyXrQvIvyLrVi9FneyumQOE8_9CA&sensor=true"></script>
+        <script src="js/maps.js"></script>
+        <script src="js/jQuery/jquery-1.11.3.min.js"></script>
+        <script src="twbs/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+        <script src="js/ovc.js"></script>
+    </body>
+</html>
