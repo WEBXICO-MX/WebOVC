46b8a6ffe9227352a18b496d03bdbe3bd51fe82b
WEBXICO-MX <weiss.uttab@gmail.com>
2015-10-21 16:02:09 -0500

    Versión inicial.

---
+++ clientes.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <title>Grupo HISA | Clientes</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="Roberto Eder Weiss Juárez">
+        <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
+        <link href="twbs/bootstrap-3.3.5-dist/css/bootstrap.min.css" rel="stylesheet"/>
+        <link href="css/shop-homepage.css" rel="stylesheet"/>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- Navigation -->
+        <?php require_once 'php/include_header.php'; ?>
+
+        <!-- Page Content -->
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-12">
+                    <div class="row carousel-holder">
+
+                        <div class="col-md-12">
+                            <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                                <ol class="carousel-indicators">
+                                    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                                </ol>
+                                <div class="carousel-inner">
+                                    <div class="item active">
+                                        <img class="slide-image" src="img/clientes/solar_turbines.JPG" alt="Solar Turbines">
+                                    </div>
+                                    <div class="item">
+                                        <img class="slide-image" src="img/clientes/uttab.jpg" alt="Universidad  Tecnológica de Tabasco">
+                                    </div>
+                                    <div class="item">
+                                        <img class="slide-image" src="img/clientes/ujat.gif" alt="Universidad Juárez Autónoma de Tabasco">
+                                    </div>
+                                    <div class="item">
+                                        <img class="slide-image" src="img/clientes/UNACAR.jpg" alt="UNACAR">
+                                    </div>
+                                    <div class="item">
+                                        <img class="slide-image" src="img/clientes/utcam.jpg" alt="UTCAM">
+                                    </div>
+                                    <div class="item">
+                                        <img class="slide-image" src="img/clientes/laboratorios_chontalpa.jpg" alt="Laboratorios Chontalpa ">
+                                    </div>
+                                </div>
+                                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                    <span class="glyphicon glyphicon-chevron-left"></span>
+                                </a>
+                                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                    <span class="glyphicon glyphicon-chevron-right"></span>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="img/clientes/solar_turbines.JPG" alt="Solar Turbines" class="img-rounded">
+                                <div class="caption">
+                                    <h4><a href="#">Solar Turbines</a>
+                                    </h4>
+                                    <p>Solar Turbines fabrica turbinas a gas industriales medianas para la generación de energía eléctrica, compresión de gas y sistemas de bombeo. <a target="_blank" href="http://www.caterpillar.com/es/company/brands/solar-turbines.html">http://www.caterpillar.com/es/company/brands/solar-turbines.html</a>.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="img/clientes/uttab.jpg" alt="Universidad  Tecnológica de Tabasco" class="img-rounded">
+                                <div class="caption">
+                                    <h4><a href="#">Universidad  Tecnológica de Tabasco</a>
+                                    </h4>
+                                    <p>En la Universidad Tecnológica de Tabasco (UTTAB) estamos ciertos de que los avances científicos y el progreso técnico deben ser empleados para satisfacer las necesidades de la sociedad, a partir de una aplicación ética y una visión humanista.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="img/clientes/ujat.gif" alt="Universidad Juárez Autónoma de Tabasco" class="img-rounded">
+                                <div class="caption">
+                                    <h4><a href="#">Universidad Juárez Autónoma de Tabasco</a>
+                                    </h4>
+                                    <p>Universidad Juárez Autónoma de Tabasco, también conocida como UJAT, es una universidad publica localizada en Villahermosa, Tabasco, México.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="img/clientes/UNACAR.jpg" alt="" class="img-rounded">
+                                <div class="caption">
+                                    <h4><a href="#">UNACAR</a>
+                                    </h4>
+                                    <p>La Universidad Autónoma del Carmen surge del Liceo Carmelita, abierto el 15 de marzo de 1854 en el actual centro histórico de la ciudad.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="img/clientes/utcam.jpg" alt="UTCAM" class="img-rounded">
+                                <div class="caption">
+                                    <h4><a href="#">UTCAM</a>
+                                    </h4>
+                                    <p>La Universidad Tecnológica de Campeche, UTCAM, es una institución de educación superior de excelencia basada en programas acreditados, procesos certificados y cuerpos académicos en consolidación, que busca la formación de profesionistas de calidad por medio de la enseñanza basada en competencias profesionales.</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-4 col-lg-4 col-md-4">
+                            <div class="thumbnail">
+                                <img src="img/clientes/laboratorios_chontalpa.jpg" alt="Laboratorios Chontalpa " class="img-rounded">
+                                <div class="caption">
+                                    <h4><a href="#">Laboratorios Chontalpa </a>
+                                    </h4>
+                                    <p>Servicios de Análisis Clínicos, Patología, Biología Molecular, Rayos X, Antidoping, Servicios integrales empresariales...</p>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <hr>
+       <!-- /.container -->
+
+        <?php require_once 'php/include_footer.php'; ?>
+
+        </div>
+        <!-- /.container -->
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDpdw9gyXrQvIvyLrVi9FneyumQOE8_9CA&sensor=true"></script>
+        <script src="js/maps.js"></script>
+        <script src="js/jQuery/jquery-1.11.3.min.js"></script>
+        <script src="twbs/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+        <script src="js/ovc.js"></script>
+    </body>
+</html>
