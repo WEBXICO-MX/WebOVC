ba30005490f9498dc98307c83890784991c00adb
WEBXICO-MX <weiss.uttab@gmail.com>
2015-10-23 10:16:33 -0500

    Se cambian las ccordenadas del mapa para que apunte Villahermosa en general.

--- js/maps.js
+++ js/maps.js
@@ -1,8 +1,9 @@
 var map;
 function initialize()
 {
-    var coordenadas = new google.maps.LatLng(18.023869, -92.920003);
-    var opt = {zoom: 16, center: coordenadas, mapTypeId: google.maps.MapTypeId.ROADMAP};
+    //var coordenadas = new google.maps.LatLng(18.023869, -92.920003);
+    var coordenadas = new google.maps.LatLng(17.989252, -92.939047);
+    var opt = {zoom: 12, center: coordenadas, mapTypeId: google.maps.MapTypeId.ROADMAP};
     var map = new google.maps.Map(document.getElementById('google_maps'), opt);
     var marker = new google.maps.Marker({position: coordenadas, animation: google.maps.Animation.DROP, icon: "img/Map-Marker-Push-Pin-1-Left-Pink-icon.png"});
     marker.setMap(map);
